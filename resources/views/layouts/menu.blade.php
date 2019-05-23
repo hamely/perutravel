3ae3db10bc8b94e7fab1004ce959416b59cce78a
hamely <sarmiento101147@gmail.com>
2019-05-23 12:39:02 -0500

    alojamiento
    alojamiento

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,4 @@
+<li class="{{ Request::is('tipoAlojamientos*') ? 'active' : '' }}">
+    <a href="{!! route('tipoAlojamientos.index') !!}"><i class="fa fa-edit"></i><span>Tipo Alojamientos</span></a>
+</li>
+
