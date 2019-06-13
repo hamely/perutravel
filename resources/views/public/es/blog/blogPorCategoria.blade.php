4b49b678a21e39906db837f69e19a63776d64180
hamely <sarmiento101147@gmail.com>
2019-06-13 11:17:48 -0500

    blog categoria detalle avance

--- resources/views/public/es/blog/blogPorCategoria.blade.php
+++ resources/views/public/es/blog/blogPorCategoria.blade.php
@@ -10,118 +10,25 @@
               <!-- Blog Post-->
               <div class="col-lg-6 mb-30">
                 <!-- Blog item-->
-                
+                @foreach($blog as $item)
                 <div class="blog-item clearfix border">
                   <!-- Blog Image-->
                   <div class="blog-media"><a href="blog-single.html">
-                      <div class="pic"><img src="/public/pic/blog/270x270/1.jpg" data-at2x="pic/blog/270x270/1@2x.jpg" alt></div></a></div>
+                      <div class="pic"><img src="{{ $item->urlimagen}}" data-at2x="pic/blog/270x270/1@2x.jpg" alt style="width:270px; height:270px;"></div></a></div>
                   <!-- blog body-->
                   <div class="blog-item-body clearfix">
                     <!-- title--><a href="blog-single.html">
-                      <h6 class="blog-title">Sed semper lacus et enim sodales</h6></a>
-                    <div class="blog-item-data">Mon, 03-23-2016</div>
+                      <h6 class="blog-title">{{$item->titulo}}</h6></a>
+                    <div class="blog-item-data">{{$item->fechaPublicacion}}</div>
                     <!-- Text Intro-->
-                    <p>Etiam maximus molestie accumsan. Sed metus sapien, fermentum nec lorem ac, tempor gravida arcu.</p><a href="blog-single.html" class="blog-button">Read more</a>
+                    <p>{{$item->descripcioncorta}}</p><a href="blog-single.html" class="blog-button">Leer más</a>
                   </div>
                 </div>
+                @endforeach()
                 <!-- ! Blog item-->
               </div>
               <!-- ! Blog Post-->
-              <!-- Blog Post-->
-              <div class="col-lg-6 mb-30">
-                <!-- Blog item-->
-                <div class="blog-item clearfix border">
-                  <!-- Blog Image-->
-                  <div class="blog-media"><a href="blog-single.html">
-                      <div class="pic"><img src="/public/pic/blog/270x270/2.jpg" data-at2x="pic/blog/270x270/2@2x.jpg" alt></div></a></div>
-                  <!-- blog body-->
-                  <div class="blog-item-body clearfix">
-                    <!-- title--><a href="blog-single.html">
-                      <h6 class="blog-title">Aenean nec urna ullamcorper</h6></a>
-                    <div class="blog-item-data">Mon, 03-23-2016</div>
-                    <!-- Text Intro-->
-                    <p>Nullam efficitur lorem eu felis rutrum elementum. Vestibulum feugiat massa id ante scelerisque blandit.</p><a href="blog-single.html" class="blog-button">Read more</a>
-                  </div>
-                </div>
-                <!-- ! Blog item-->
-              </div>
-              <!-- ! Blog Post-->
-              <!-- Blog Post-->
-              <div class="col-lg-6 mb-30">
-                <!-- Blog item-->
-                <div class="blog-item clearfix border">
-                  <!-- Blog Image-->
-                  <div class="blog-media"><a href="blog-single.html">
-                      <div class="pic"><img src="/public/pic/blog/270x270/3.jpg" data-at2x="pic/blog/270x270/3@2x.jpg" alt></div></a></div>
-                  <!-- blog body-->
-                  <div class="blog-item-body clearfix">
-                    <!-- title--><a href="blog-single.html">
-                      <h6 class="blog-title">Etiam sit amet est et massa</h6></a>
-                    <div class="blog-item-data">Mon, 03-23-2016</div>
-                    <!-- Text Intro-->
-                    <p>urabitur rhoncus sem et eros pharetra euismod. Ut commodo sem magna, in congue ex vestibulum ut.</p><a href="blog-single.html" class="blog-button">Read more</a>
-                  </div>
-                </div>
-                <!-- ! Blog item-->
-              </div>
-              <!-- ! Blog Post-->
-              <!-- Blog Post-->
-              <div class="col-lg-6 mb-30">
-                <!-- Blog item-->
-                <div class="blog-item clearfix border">
-                  <!-- Blog Image-->
-                  <div class="blog-media"><a href="blog-single.html">
-                      <div class="pic"><img src="/public/pic/blog/270x270/4.jpg" data-at2x="pic/blog/270x270/4@2x.jpg" alt></div></a></div>
-                  <!-- blog body-->
-                  <div class="blog-item-body clearfix">
-                    <!-- title--><a href="blog-single.html">
-                      <h6 class="blog-title">Integer venenatis leo et dapibus tristique</h6></a>
-                    <div class="blog-item-data">Mon, 03-23-2016</div>
-                    <!-- Text Intro-->
-                    <p>Cras tellus orci, ultricies at ornare rhoncus, vehicula ac nibh. Vivamus consectetur eget diam ac mollis.</p><a href="blog-single.html" class="blog-button">Read more</a>
-                  </div>
-                </div>
-                <!-- ! Blog item-->
-              </div>
-              <!-- ! Blog Post-->
-              <!-- Blog Post-->
-              <div class="col-lg-6 mb-30">
-                <!-- Blog item-->
-                <div class="blog-item clearfix border">
-                  <!-- Blog Image-->
-                  <div class="blog-media"><a href="blog-single.html">
-                      <div class="pic"><img src="/public/pic/blog/270x270/5.jpg" data-at2x="pic/blog/270x270/5@2x.jpg" alt></div></a></div>
-                  <!-- blog body-->
-                  <div class="blog-item-body clearfix">
-                    <!-- title--><a href="blog-single.html">
-                      <h6 class="blog-title">Nulla eget purus at nibh scelerisque mollis</h6></a>
-                    <div class="blog-item-data">Mon, 03-23-2016</div>
-                    <!-- Text Intro-->
-                    <p>Ut tincidunt sapien consectetur elit mollis tincidunt. Integer vel ex nec quam gravida posuere vulputate a diam.</p><a href="blog-single.html" class="blog-button">Read more</a>
-                  </div>
-                </div>
-                <!-- ! Blog item-->
-              </div>
-              <!-- ! Blog Post-->
-              <!-- Blog Post-->
-              <div class="col-lg-6 mb-30">
-                <!-- Blog item-->
-                <div class="blog-item clearfix border">
-                  <!-- Blog Image-->
-                  <div class="blog-media"><a href="blog-single.html">
-                      <div class="pic"><img src="/public/pic/blog/270x270/6.jpg" data-at2x="pic/blog/270x270/6@2x.jpg" alt></div></a></div>
-                  <!-- blog body-->
-                  <div class="blog-item-body clearfix">
-                    <!-- title--><a href="blog-single.html">
-                      <h6 class="blog-title">Maecenas id ligula sed ex feugiat lobortis</h6></a>
-                    <div class="blog-item-data">Mon, 03-23-2016</div>
-                    <!-- Text Intro-->
-                    <p>Nam elementum leo ex, nec lacinia ligula malesuada nec. Phasellus bibendum tempor leo, a accumsan justo molestie at.</p><a href="blog-single.html" class="blog-button">Read more</a>
-                  </div>
-                </div>
-                <!-- ! Blog item-->
-              </div>
-              <!-- ! Blog Post-->
+
             
            
             </div>
